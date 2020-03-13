db459220ab60f9d876cdf950359e8852bc4ddeed
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-13 19:34:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,11 +1,15 @@
 require 'pry'
 class TicTacToe
-
-  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-
-  def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
+  WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+  ]
 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -16,41 +20,122 @@ class TicTacToe
   end
 
   def input_to_index(user_input)
-    user_input.to_i-1
+  	user_input.to_i - 1
   end
 
-  def move(index, piece)
-    @board[index] = piece
+  def move(index, value)
+    @board[index] = value
   end
 
+  # Helper Method
   def position_taken?(index)
-    @board[index] != " "
+     !(@board[index] == " ")
   end
 
+  #valid_move?
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(index)
+  	#if move is valid
+  	if position_taken?(index) == false && index.between?(0,8) == true
+  		return true
+  	#if move is invalid
+  	elsif position_taken?(index) == true
+  		return false
+  	elsif index.between?(0, 8) == false 
+  		return false
+  	end
   end
 
+  #turn_count
   def turn_count
-    @board.count { | space | space != " " }
+  	turns = 0
+  	@board.each do |turn|
+  		if turn == "X"
+  			turns += 1
+  		elsif turn == "O"
+  			turns += 1
+  		end
+  	end
+  	return turns
   end
 
+  #current_player
   def current_player
-    turn_count.even? ? "X" : "O"
+  	return turn_count.even? == true ? "X" : "O"
   end
 
+  #turn
   def turn
-    puts "Choose your move wisely (1-9):"
-    user_input = gets.strip
-    index = input_to_index(user_input)
-    if valid_move?(index)
-        piece = current_player
-        move(index, piece)
-    else
-      "Please select a valid choice."
-      turn
-    end
-    display_board
+  	puts "Please enter 1-9:"
+  	user_input = gets.chomp
+  	index = input_to_index(user_input)
+  	if valid_move?(index) == true && current_player == "X"
+  			move(index, value = "X")
+  			puts display_board
+  	elsif valid_move?(index) == true && current_player == "O"
+  			move(index, value = "O")
+  			puts display_board
+  	elsif valid_move?(index) == false
+  		puts "Please enter 1-9:"
+  		input = gets.chomp
+  	else 
+  		puts "Please enter 1-9:"
+  		input = gets.chomp
+  	end
   end
 
-end
+  #won?
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+    end
+  end
+
+  #full
+  def full?
+  	@board.all? do |index|
+  		 index == "X" || index == "O"
+  		end
+  end
+
+  #draw
+  def draw?
+  		if won? 
+  			return false
+  		elsif full? == false 
+  			return false 
+  		else 
+  			return true
+  		end
+  end
+
+  #over
+  def over?
+  		if won? || draw? == true
+  			return true
+  		else
+  			return false
+  		end
+  end
+
+  #winner
+  def winner
+  	winning_combo = won?
+  	 if winning_combo
+  	    return @board[winning_combo[0]]
+  	 end
+  end
+
+  #play
+  def play
+      while over? == false
+      	turn
+      end
+
+      if draw?
+      	puts "Cat's Game!"
+      elsif winner == "X" 
+      	puts "Congratulations X!"
+      elsif winner == "O"
+      	puts "Congratulations O!"
+      end
+  end
